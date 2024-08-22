ecabe91b905e780021bd221da4c08601a47dba64
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-08-22 22:29:06 +0530

    Update callback.py

--- ChampuXMusic/plugins/admins/callback.py
+++ ChampuXMusic/plugins/admins/callback.py
@@ -37,7 +37,6 @@ from ChampuXMusic.utils.decorators.language import languageCB
 from ChampuXMusic.utils.formatters import seconds_to_min
 from ChampuXMusic.utils.inline import (
     close_markup,
-    panel_markup_1,
     panel_markup_2,
     panel_markup_3,
     panel_markup_4,
